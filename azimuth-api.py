fecb67219773f40cbb14d6adfb5ab68b31fdc92c
kgao <kevingao@microsoft.com>
2017-06-29 13:47:07 -0400

    update flask verison

--- azimuth-api.py
+++ azimuth-api.py
@@ -14,7 +14,7 @@ def home():
 @app.route("/test")
 def test():
     sequences = np.array(['ACAGCTGATCTCCAGATATGACCATGGGTT', 'CAGCTGATCTCCAGATATGACCATGGGTTT', 'CCAGAAGTTTGAGCCACAAACCCATGGTCA'])
-    amino_acid_cut_positions = np.array([1, 1, 2])
+    amino_acid_cut_positions = np.array([2, 2, 4])
     percent_peptides = np.array([0.18, 0.18, 0.35])
     predictions = azimuth.model_comparison.predict(sequences, amino_acid_cut_positions, percent_peptides)
     for i, prediction in enumerate(predictions):
